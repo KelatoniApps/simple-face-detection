b1e9c4f07b0b510f514f194a051b1e852b8f1b4b
sineco <sineco@gmail.com>
2015-08-10 21:43:31 -0400

    Added face detection files

---
+++ build/CMakeFiles/demoFaceDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/demoFaceDetection.dir/demoFaceDetection.cpp.o"
+  "demoFaceDetection.pdb"
+  "demoFaceDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/demoFaceDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
